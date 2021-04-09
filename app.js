6f1e4a2a05decd522508003f00110a29ab8ad306
Prakhar <prakhar.gupta.1106@gmail.com>
2021-04-10 02:52:52 +0530

    index.ejs added | css,js,asset added

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const port = 5000;
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
+
+
+app.get('/',(req,res)=>{
+    res.render("index.ejs");
+});
+
 app.listen(port, () => {
 
     console.log(`Server is up and Running on localhost ${port}`);
-});
-
-app.get('/',(req,res)=>{
-    res.send("hello world");
 });
