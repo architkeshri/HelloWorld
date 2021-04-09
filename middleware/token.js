8791b10d9e8b7684c50e096e31e2a9e974340021
Archit <architkeshri4@gmail.com>
2021-04-10 02:07:43 +0530

    login feature done

--- middleware/token.js
+++ middleware/token.js
@@ -1,1 +1,24 @@
-//authentication goes here
+const express = require('express');
+const { JsonWebTokenError } = require('jsonwebtoken');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+function checkToken(req, res, next){
+    const authcookie = req.cookies.authcookie;
+
+    jwt.verify(authcookie,process.env.JWT_SECRET,(err,data)=>{
+        //console.log(data);
+        if(err){
+            res.redirect('/login');
+        }
+        else if(data.user_id){
+            req.user_id = data.user_id;
+            next();
+        }
+
+    });
+
+
+}
+
+module.exports = checkToken;
