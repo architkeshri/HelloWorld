1b8e17e9559296f1ec03c2587f10706ed8179a50
Archit <architkeshri4@gmail.com>
2021-04-10 12:15:51 +0530

    answer and question backend

--- routes/pages.js
+++ routes/pages.js
@@ -59,5 +59,153 @@ router.get('/dashboard',checkToken, (req, res) => {
         }
     });
 });
+router.get('/questions-:id', checkToken,(req, res)=>{
 
+    var id = req.params.id;
+    console.log("Tag id is " + id);
+    var query = `SELECT * FROM tag_details WHERE tag_id='${id}';`
+
+    myConn.query(query,(err,category)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+
+            var query2 = `SELECT * FROM question_details WHERE q_tag='${id}';`
+
+            myConn.query(query2,(err,questionslist)=>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    var data = {category,questionslist};
+                    console.log(data);
+                   // console.log(category);
+                   // console.log(questionslist);
+                    res.render('questions',data);
+                }
+            });
+
+        }
+    });
+
+   
+
+});
+
+
+
+router.post('/askquestion-:tag_id', checkToken,(req, res)=>{
+
+    var tag_id = req.params.tag_id;
+    var ques_text = req.body.ques_text;
+    console.log(tag_id);
+
+    var query = `INSERT INTO question_details SET user_id = '${req.user_id}', ques_text = '${ques_text}', q_tag = '${tag_id}';`
+
+    myConn.query(query,(err,result) =>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect(`questions-${tag_id}`);
+        }
+    });
+    
+});
+
+router.post('/profile-update-:id',(req,res)=>{
+
+    var contact = req.body.phone;
+    var fb_link = req.body.facebook;
+    var git_link = req.body.github;
+    var lin_link = req.body.linkedin;
+    var id = req.params.id;
+
+    var query = `UPDATE user_details SET contact = ?, fb_link = ?,git_link = ?, lin_link = ? WHERE user_id = ? `;
+
+    myConn.query(query,[contact,fb_link,git_link,lin_link,id],(err,result)=>{
+
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log(result);
+            console.log(id);
+  
+            res.redirect('/dashboard');
+        }
+   });
+    
+});
+
+
+
+router.get('/answers-:id', checkToken,(req, res)=>{
+
+    var id = req.params.id;
+    console.log("Ques id is " + id);
+    var query = `SELECT * FROM question_details WHERE ques_id='${id}';`
+
+    myConn.query(query,(err,questions)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+
+            var query2 = `SELECT * FROM answer_details WHERE ques_id='${id}';`
+
+            myConn.query(query2,(err,answerslist)=>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    // // var query3 = `SELECT * FROM user_details WHERE user_id='${ans_by}';`
+
+                    // myConn.query(query3,(err,userdetails)=>{
+                    //     if(err){
+                    //         console.log(err);
+                    //     }
+                    //     else{
+                    //         var data = {questions,answerslist, userdetails};
+                    //         console.log(data);
+                    //         console.log(questions);
+                    //         console.log(answerslist);
+                    //         console.log(userdetails);
+                    //         res.render('answerthread',data);
+                    //     }
+                    // });
+        
+                    var data = {questions,answerslist};
+                    console.log(data);
+                    console.log(questions);
+                    console.log(answerslist);
+                    res.render('answerthread',data);
+                }
+            });
+
+        }
+    });
+
+});
+
+
+router.post('/addanswer-:ques_id', checkToken,(req, res)=>{
+
+    var ques_id = req.params.ques_id;
+    var ans_text = req.body.ans_text;
+    console.log(ques_id);
+
+    var query = `INSERT INTO answer_details SET ans_text = '${ans_text}', ques_id = '${ques_id}';`
+
+    myConn.query(query,(err,result) =>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect(`answers-${ques_id}`);
+        }
+    });
+    
+});
 module.exports = router;
